885ed02538bb98e6fc392b6bdc52bc53cad54532
Kaparthy Reddy <166050493+KaparthyReddy@users.noreply.github.com>
2026-09-05 14:26:59 +0530

    Create V3__seed_storage_units.sql

---
+++ src/main/resources/db/migration/V3__seed_storage_units.sql
@@ -0,0 +1,4 @@
+INSERT INTO storage_units (name, location, unit_type) VALUES
+    ('Freezer A', 'Bengaluru DC', 'FREEZER'),
+    ('Refrigerator B', 'Bengaluru DC', 'REFRIGERATOR'),
+    ('Warehouse C', 'Chennai Hub', 'WAREHOUSE');
