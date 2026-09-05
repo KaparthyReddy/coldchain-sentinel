7c7430bdec0c7aeb309be678a23ddac6f103b742
Kaparthy Reddy <166050493+KaparthyReddy@users.noreply.github.com>
2026-09-05 13:31:52 +0530

    Create UserRepository.java

---
+++ src/main/java/com/coldchainsentinel/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.coldchainsentinel.repository;
+
+import com.coldchainsentinel.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
