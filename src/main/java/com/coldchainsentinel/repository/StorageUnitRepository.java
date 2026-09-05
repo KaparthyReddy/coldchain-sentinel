d8d621d498b9321d8e12709415eac860efbfb670
Kaparthy Reddy <166050493+KaparthyReddy@users.noreply.github.com>
2026-09-05 13:32:25 +0530

    Create StorageUnitRepository.java

---
+++ src/main/java/com/coldchainsentinel/repository/StorageUnitRepository.java
@@ -0,0 +1,7 @@
+package com.coldchainsentinel.repository;
+
+import com.coldchainsentinel.model.StorageUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StorageUnitRepository extends JpaRepository<StorageUnit, Long> {
+}
